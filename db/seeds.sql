b228cdf84d4b7c22345ef2179f13d6d7c1445b5c
Em Coraccio <emcoraccio@gmail.com>
2019-09-29 12:47:44 -0400

    created burgers_db

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+ INSERT INTO burgers (burger_name) VALUES ('Classic');
+ INSERT INTO burgers (burger_name) VALUES ('Cajun');
+ INSERT INTO burgers (burger_name) VALUES ('Cheddar Bacon');
+ INSERT INTO burgers (burger_name, devoured) VALUES ('Double', true);
