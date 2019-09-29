1815815e4401b81006226d1b454515ebd885da7d
Em Coraccio <emcoraccio@gmail.com>
2019-09-29 14:14:37 -0400

    created router

---
+++ controllers/burgers_controller.js
@@ -0,0 +1,39 @@
+const express = require("express");
+let router = express.Router();
+
+const burger = require("../models/burger");
+
+router.get("/", function(req, res){
+  burger.selectAll(function(data) {
+    let dataObj = {burgers: data};
+    console.log(dataObj);
+    res.render("index", dataObj);
+  });
+});
+
+router.post("/api/burgers", function(req, res) {
+  burger.insertOne([
+    "burger_name", "devoured"
+  ], [req.body.burger_name, req.body.devoured], function(result){
+    res.json({ id: result.inserId });
+  });
+});
+
+router.put("api/burgers/:id", function(req, res) {
+  let condition = `id = ${req.params.id}`;
+
+  console.log("condition", condition);
+
+  burger.updateOne({
+    devoured: req.body.devoured
+  }, condition, function(result) {
+    if(result.changedRows == 0) {
+      return res.status(404).end();
+    }
+    else {
+      res.status(200).end();
+    }
+  });
+});
+
+module.exports = router;
